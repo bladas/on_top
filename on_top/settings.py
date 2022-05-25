edfcdf0c8b5fe230f958aafc7bf158bafd36d388
bladas <daskevichvladislav25@gmail.com>
2022-05-25 21:56:16 +0300

    added authtoken

--- on_top/settings.py
+++ on_top/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
     'dashboard.apps.DashboardConfig',
     'accounts.apps.AccountsConfig'

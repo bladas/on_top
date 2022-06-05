f1aaa6d1d51283ff0ede4f468816f9ba4ea7b172
bladas <daskevichvladislav25@gmail.com>
2022-06-05 20:28:13 +0300

    added modal admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from dashboard.models import Goal, GoalMentor, MentorComment, DiaryComment
+from dashboard.models import Goal, GoalMentor, MentorComment, DiaryComment, SubGoal, GoalReminding
 
 admin.site.register(Goal)
 admin.site.register(GoalMentor)
 admin.site.register(MentorComment)
 admin.site.register(DiaryComment)
+admin.site.register(GoalReminding)
+admin.site.register(SubGoal)

f4730052bbbb58a77c7a13dee98796fc735e0488
bladas <daskevichvladislav25@gmail.com>
2022-06-06 00:57:41 +0300

    changes

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -9,7 +9,7 @@ from telegram_bot.service import send_text_message
 def send_message():
     try:
         for reminding in GoalReminding.objects.filter(is_active=True):
-            if reminding.date.hour == datetime.now().hour:
-                send_text_message(chat_id=reminding.user.chat_id, text=reminding.text)
+            # if reminding.date.hour == datetime.now().hour:
+            send_text_message(chat_id=reminding.user.chat_id, text=reminding.text)
     except:
         logging.warning("SENDING ERROR")

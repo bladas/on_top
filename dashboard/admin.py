bd54b5a2f657f59203e229e8c7688a8a54d8d467
bladas <daskevichvladislav25@gmail.com>
2022-06-06 10:21:07 +0300

    update admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from dashboard.models import Goal, GoalMentor, MentorComment, DiaryComment, SubGoal, GoalReminding
+from dashboard.models import Goal, GoalMentor, MentorComment, DiaryComment, SubGoal, GoalReminding, SubGoalCompletion
 
 admin.site.register(Goal)
 admin.site.register(GoalMentor)
@@ -9,3 +9,4 @@ admin.site.register(MentorComment)
 admin.site.register(DiaryComment)
 admin.site.register(GoalReminding)
 admin.site.register(SubGoal)
+admin.site.register(SubGoalCompletion)
